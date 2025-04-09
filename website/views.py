71e3ca80c22f97982e6c6d792f0a59bcb18d0bef
Jonathan Ho <jonathanho@Jonathans-MacBook-Pro.local>
2025-04-09 02:06:41 -0400

    Clean up

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from .app.bgg import search_via_name, search_via_id
+from .src.bgg import search_via_name, search_via_id
 from .models import Game
 from . import db
 import json
